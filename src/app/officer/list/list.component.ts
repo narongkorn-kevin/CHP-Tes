dc39871509f66f2738f2c656158fa94b3971d4b6
(100231) ชวลิต พรหมลัทธิ์ <100231@mnl.local>
2021-07-28 16:20:59 +0700

    users

--- src/app/officer/list/list.component.ts
+++ src/app/officer/list/list.component.ts
@@ -9,14 +9,7 @@ import {
   ViewChild,
   OnDestroy,
 } from '@angular/core';
-import { Router, NavigationExtras } from '@angular/router';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
-import { MatDialog } from '@angular/material/dialog';
-import { Subject } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { DataTableDirective } from 'angular-datatables';
-import { CustomerResponse } from '@app/shared/models/base.interface';
+
 
 declare var $: any;
 const user = JSON.parse(localStorage.getItem('user')) || null;
@@ -28,27 +21,8 @@ const user = JSON.parse(localStorage.getItem('user')) || null;
 })
 export class ListComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'role', 'username', 'actions'];
-  dataSource = new MatTableDataSource();
-
-  /*httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${user.token}`,
-    }),
-  }; */
-
-  public dtOptions: DataTables.Settings = {};
-
-  private destroy$ = new Subject<any>();
   public dataRow: any[];
 
-  @ViewChild(DataTableDirective)
-  dtElement!: DataTableDirective;
-  
-
-  @ViewChild(MatSort) sort: MatSort;
-  constructor() {}
 
 
   ngOnInit(): void {
